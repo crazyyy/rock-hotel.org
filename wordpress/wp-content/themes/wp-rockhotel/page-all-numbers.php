497f9aced4a2d4d549edb7c3be9f93736b0d981a
Vitaly A <crazyyy@gmail.com>
2015-03-11 10:43:44 +0200

    libma

---
+++ wordpress/wp-content/themes/wp-rockhotel/page-all-numbers.php
@@ -0,0 +1,42 @@
+<?php /* Template Name: Numbers Main */ get_header(); ?>
+	
+	<!-- section -->
+	<section role="main">
+		<!-- article -->
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class('mainer clearfix'); ?>>
+			<h1 class="page-title inner-title"><?php the_title(); ?></h1>
+
+			<?php the_content(); ?>
+
+			<?php $posts = get_field('numbers');
+		        if( $posts ): ?>
+		        <ul class="numbers-catalog clearfix">
+		          <?php foreach( $posts as $p ): // variable must NOT be called $post (IMPORTANT) ?>
+		            <li>
+						<a href="<?php echo get_permalink( $p->ID ); ?>">
+						<?php echo get_the_post_thumbnail( $p->ID  ); ?>
+						<h2><?php echo get_the_title( $p->ID ); ?></h2>
+
+						<p><?php echo get_field('description', $p->ID); ?></p>
+
+						<?php if(get_field('price', $p->ID))	{ echo '<span class="price">' . get_field('price', $p->ID) . '</span>';	} ?>
+						</a>
+					 	<form action="">
+					 		<input type="text" placeholder="20.09.2013">
+					 		<input type="text" placeholder="20.09.2013">
+					 		<button>забронировать</button>
+					 	</form>
+		            </li>
+		          <?php endforeach; ?>
+		        </ul><!-- numbers-catalog -->
+		      <?php endif; ?>
+
+		<?php endwhile; else: // If 404 page error ?>
+			<h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+		<?php endif; ?>
+		</div>
+		<!-- /article -->
+	</section>
+	<!-- /section -->
+<?php get_footer(); ?>
--- wordpress/wp-content/themes/wp-rockhotel/page-reviews.php
+++ wordpress/wp-content/themes/wp-rockhotel/page-reviews.php
@@ -4,35 +4,35 @@
 	<section role="main">
 		<!-- article -->
 
+		<h1 class="lazy-selector">Отзывы о нас</h1>
 
-	<?php 
-		query_posts( array(
-			'post_type' => reviews,
-			'showposts' =>10 )
-		); 
-	?>
-	<?php while ( have_posts() ) : the_post(); ?>
+		<ul class="all-review-page">
+			<?php
+			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			$temp = $wp_query;
+			$wp_query= null;
+			query_posts('post_type=reviews'.'&showposts=10'.'&paged='.$paged);
+			while (have_posts()) : the_post();?>
+
+			<li>
+				<?php the_content(); ?>
+				<p class="user-name"><?php the_title(); ?><span class="user-from user-from-<?php the_field('country'); ?>"><?php 
+				$field = get_field_object('country');
+				$value = get_field('country');
+				$label = $field['choices'][ $value ];
+				echo $label; ?></span></p>
+			</li>
 
 
-
-
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
-
-			<h1 class="page-title inner-title"><?php the_title(); ?></h1>
-
-			<?php the_content(); ?>
-			
-
-
-		</article>
-
-	<?php endwhile; ?>
-	<?php wp_reset_query(); ?>
-<?php get_template_part('pagination'); ?>
+			<?php endwhile; ?>
+		</ul><!-- all-review-page -->
+		<div class="paginugets">
+			<?php html5wp_pagination(); ?>		
+		</div>
+		<!-- /.paginugets -->
+		<?php $wp_query = null; $wp_query = $temp;?>
 
 	</section>
 	<!-- /section -->
-	<?php get_template_part('user-reviews'); ?>
 	<?php get_template_part('select-rooms'); ?>
 <?php get_footer(); ?>
